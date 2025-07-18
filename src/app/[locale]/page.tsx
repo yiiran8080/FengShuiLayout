26a2cc361b42facf90492e50fb41c1fe55475206
Michael Ng <hoihoi1083@gmail.com>
2025-07-18 19:05:52 +0800

    facebook homepage update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,6 +12,45 @@ import Desire from "@/components/home/Desire";
 import Theory from "@/components/free/theory"; // Changed import
 
 import { get } from "@/lib/ajax";
+
+// Add metadata for homepage
+export const metadata = {
+	title: "HarmoniQ 風水分析 - 打造和諧家居布局",
+	description:
+		"🏠✨ 專業風水分析服務，幫您優化家居布局，提升生活品質。現在還有特別優惠，立即體驗！",
+
+	// Open Graph
+	openGraph: {
+		title: "HarmoniQ 風水分析 - 打造和諧家居布局",
+		description:
+			"🏠✨ 發現了一個超棒的風水分析網站！HarmoniQ幫我優化家居布局，讓生活更和諧幸福！現在還有特別優惠，快來試試看吧！我在HarmoniQ風水測算領取專屬家居風水報告，分享或發佈貼文到Facebook領取優惠碼即可獲得市場價一折限時優惠，立即分享給親朋好友一起享受專屬折扣價格🙌🏻",
+		url: "https://www.harmoniqfengshui.com/zh-TW",
+		siteName: "HarmoniQ 風水分析",
+		images: [
+			{
+				url: "https://www.harmoniqfengshui.com/images/hero/Facebookpost.jpg", // 使用 Facebook 專用圖片
+				width: 1200,
+				height: 630,
+				alt: "HarmoniQ 風水分析 - 專業家居布局優化",
+			},
+		],
+		locale: "zh_TW",
+		type: "website",
+		appId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID || "687233617481228",
+	},
+
+	// Twitter Card
+	twitter: {
+		card: "summary_large_image",
+		title: "HarmoniQ 風水分析 - 打造和諧家居布局",
+		description:
+			"🏠✨ 發現了一個超棒的風水分析網站！HarmoniQ幫我優化家居布局，讓生活更和諧幸福！現在還有特別優惠，快來試試看吧！我在HarmoniQ風水測算領取專屬家居風水報告，分享或發佈貼文到Facebook領取優惠碼即可獲得市場價一折限時優惠，立即分享給親朋好友一起享受專屬折扣價格🙌🏻",
+		images: [
+			"https://www.harmoniqfengshui.com/images/hero/Facebookpost.jpg",
+		],
+	},
+};
+
 export default function Home() {
 	return (
 		<>
